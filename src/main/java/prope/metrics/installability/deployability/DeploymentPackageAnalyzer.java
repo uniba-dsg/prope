4ba92fd95c577d5a089c77735f6019da15285f2e
Joerg Lenhard <joerg.lenhard@uni-bamberg.de>
2014-12-10 15:57:13 +0100

    Transform FileAnalyzer to single abstract method interface

--- src/main/java/prope/metrics/installability/deployability/DeploymentPackageAnalyzer.java
+++ src/main/java/prope/metrics/installability/deployability/DeploymentPackageAnalyzer.java
@@ -327,9 +327,5 @@ public class DeploymentPackageAnalyzer implements FileAnalyzer {
 		numberOfServices = 0;
 		descriptorPaths = new LinkedList<Path>();
 	}
-
-	@Override
-	public void traversalCompleted() {
-		// Not needed here
-	}
+	
 }
