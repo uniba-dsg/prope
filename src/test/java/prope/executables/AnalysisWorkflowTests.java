00b5a38382ae8047167dfa29ea6b91d508467a20
Joerg Lenhard <joerg.lenhard@uni-bamberg.de>
2015-06-23 10:44:32 +0200

    Add integration test for deployability analysis
    Former-commit-id: f777ccc59fe8f6f05107b7d53a5bc42e51891daf

--- src/test/java/prope/executables/AnalysisWorkflowTests.java
+++ src/test/java/prope/executables/AnalysisWorkflowTests.java
@@ -1,16 +1,61 @@
 package prope.executables;
 
+import static org.junit.Assert.assertEquals;
+
 import java.nio.file.Paths;
+import java.util.List;
 
 import org.junit.Test;
 
+import prope.reporting.ReportEntry;
+
 public class AnalysisWorkflowTests {
+	
+	private AnalysisWorkflow sut;
+	
+	private final String deployabilityResourcesPath = "src/test/resources/installability/deployment/";
 
 	@Test(expected = IllegalArgumentException.class)
 	public void testNonExistingDirectory() {
-		@SuppressWarnings("unused")
-		AnalysisWorkflow sut = new AnalysisWorkflow(Paths.get("foobar"),
+		sut = new AnalysisWorkflow(Paths.get("foobar"),
 				AnalysisType.DEPLOYABILITY);
 	}
+	
+	@Test
+	public void deployabilityAnalysis(){
+		sut = new AnalysisWorkflow(Paths.get(deployabilityResourcesPath + "xmlDescriptorWithText.zip"),
+				AnalysisType.DEPLOYABILITY);
+		
+		List<ReportEntry> results = sut.start().getEntries();
+		assertEquals(results.size(),1);
+		
+		ReportEntry entry = results.get(0);
+		assertEquals(14, getDeploymentEffort(entry));
+		assertEquals(2, getEffortOfPackageConstruction(entry));
+		assertEquals(12, getDescriptorComplexity(entry));
+	}
 
+	private int getDeploymentEffort(ReportEntry entry) {
+		if (entry != null) {
+			return Integer.parseInt(entry.getVariableValue("DE"));
+		} else {
+			return 0;
+		}
+	}
+
+	private int getEffortOfPackageConstruction(ReportEntry entry) {
+		if (entry != null) {
+			return Integer.parseInt(entry.getVariableValue("EPC"));
+		} else {
+			return 0;
+		}
+	}
+
+	private int getDescriptorComplexity(ReportEntry entry) {
+		if (entry != null) {
+			return Integer.parseInt(entry.getVariableValue("DDS"));
+		} else {
+			return 0;
+		}
+	}
 }
